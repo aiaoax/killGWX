c977fdf4b0ea01279737e0aaab7abe0764626bc3
aiaoax <aiaoax@aiaoax.net>
2016-05-18 21:09:00 +0900

    201605082108

---
+++ killgwx/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Threading;
+namespace killgwx{
+	class Program{
+		static void Main(string[] args){
+			while(true){
+				try {
+					var kilp=Process.GetProcessesByName("GWX");
+					foreach(var item in kilp){
+						item.Kill();
+					}
+				}
+				catch {}
+				Thread.Sleep(100000);
+			}
+		}
+	}
+}
